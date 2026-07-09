be1f3f04cdc6139cce73a27f3a548369db4bd518
Deepraj Pagare <99066501+Deepraj0502@users.noreply.github.com>
2026-07-09 12:52:04 +0530

    Update ZipEngine.ts

--- src/modules/wrapper-generator/engines/ZipEngine.ts
+++ src/modules/wrapper-generator/engines/ZipEngine.ts
@@ -1,23 +1,11 @@
 import JSZip from "jszip";
-import templateZip from "../../../assets/template/thirdPartyTestWrapper_expDS.zip?url";
+import { TEMPLATE_BYTES } from "../assets/template";
 
 export default class ZipEngine {
   private zip: JSZip | null = null;
 
   async loadTemplate(): Promise<JSZip> {
-    const response = await fetch(templateZip);
-
-    console.log("URL:", templateZip);
-    console.log("Status:", response.status);
-
-    const firstBytes = await response.clone().text();
-    console.log("TEXT:", firstBytes.substring(0,200));
-    // if (!response.ok) {
-    //   throw new Error(`Unable to load template from ${templatePath}.`);
-    // }
-
-    const buffer = await response.arrayBuffer();
-    this.zip = await JSZip.loadAsync(buffer);
+    this.zip = await JSZip.loadAsync(TEMPLATE_BYTES);
     return this.zip;
   }
 
